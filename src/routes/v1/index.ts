82545fd10af5778bfff9f107bce1929dc75ace7d
vuthienanh <Vu_Thien_Anh@mckinsey.com>
2023-03-07 23:27:22 +0700

    upload solution

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express'
 
+import { authRouter } from '@/routes/v1/auth.route'
+import { userRouter } from './user.route'
+
 const router = Router()
+
+router.use('/auth', authRouter)
+router.use('/users', userRouter)
 
 export default router
